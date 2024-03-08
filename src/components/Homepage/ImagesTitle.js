5cefc100f64cd7729f1d219e596a92dd6b09b984
Georgeshayek <georgeshayek85@gmail.com>
2024-03-08 11:43:20 +0200

    width fixed

--- src/components/Homepage/ImagesTitle.js
+++ src/components/Homepage/ImagesTitle.js
@@ -3,7 +3,7 @@ const { default: Image } = require("next/image")
 const Images=(props)=>{
     return(
         <div className=" py-2">
-            <Image width={350} height={200} className="rounded-xl" src={props.image} alt="image"/>
+            <Image  width={600} height={200}    sizes='1000vw' className="rounded-xl object-fit w-full" src={props.image}  alt="image"/>
             <h1 className="text-[#ffffff] py-1 font-bold">{props.title}</h1>
             <p className="text-gray-300">{props.description}</p>
         </div>
