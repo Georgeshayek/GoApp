c1ff7103221b0b7e1c8b72ce0485edca4ec81b3d
Georgeshayek <georgeshayek85@gmail.com>
2024-03-13 12:09:05 +0200

    about swiper fixes1

--- src/components/About/swipers.js
+++ src/components/About/swipers.js
@@ -11,39 +11,32 @@ import AboutItem from './aboutItem';
 import { useMediaQuery } from "react-responsive";
 
 const AboutSwiper=(props) => {
-    const [activeIndex, setActiveIndex] = useState(0);
+    const [activeIndex, setActiveIndex] = useState(1);
     const isMaxLg = useMediaQuery({ maxWidth: 1023 });
     const isMaxLg2 = useMediaQuery({ minWidth: 1023 });
     
-    // useEffect(()=>{
-    //   if(isMaxLg)
-    //   setActiveIndex(prev=>prev-1); console.log("1")},[isMaxLg]);
-    // useEffect(()=>{
-    //   if(isMaxLg2)
-    //   setActiveIndex(prev=>prev+1); console.log("1")},[isMaxLg2]);
-    //   useEffect(()=>setActiveIndex(1),[])
+   
 
   return (
     <div className="container mx-auto">
     <Swiper
     breakpoints={{
       1023:{slidesPerView:3},
-      768:{slidesPerView:1},
+      768:{slidesPerView:1}
       
     }}
-    onBreakpoint={(swiper)=>swiper.getBreakpoint.length>768?setActiveIndex(realIndex+1):setActiveIndex(realIndex)}
       modules={[Navigation, Pagination, Scrollbar, A11y]}
       spaceBetween={30}
-      //slidesPerView={isMaxLg?1:3}
+      initialSlide={1}
+      centeredSlides
       navigation
       
-      onSwiper={(swiper) => console.log(swiper.realIndex)}
-      onSlideChange={(swiper) =>setActiveIndex( swiper.realIndex)}
-   
-   >
+      onSwiper={(swiper) => console.log(swiper.activeIndex)}
+      onSlideChange={(swiper) =>setActiveIndex(swiper.activeIndex)}
+    >
         
         {props.data.map((mov,index)=>{  
-    return   ( <SwiperSlide   ><AboutItem title={mov.title} description={mov.description} image={mov.image} active={index === activeIndex}/></SwiperSlide>)
+    return   ( <SwiperSlide activeIndex ><AboutItem title={mov.title} description={mov.description} image={mov.image} active={index === activeIndex}/></SwiperSlide>)
         })}
       
     
