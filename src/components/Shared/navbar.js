a1600abccc88923ac4f81fba861ee37accc40e75
Georgeshayek <georgeshayek85@gmail.com>
2024-03-15 13:35:01 +0200

    navbar fix1

--- src/components/Shared/navbar.js
+++ src/components/Shared/navbar.js
@@ -19,12 +19,12 @@ const Navbar=()=>{
                     <Link href="/"><Image src="/logo.png" alt=" Logo" width={100} height={50}/></Link>
                 </div>
                 <button id="burger-btn" className="  block text-white lg:hidden" onClick={toggleShow}>&#9776;</button>
-                <ul className={`flex justify-center  max-lg:w-full   max-lg:flex-col ${!show ? 'max-lg:hidden' : 'max-lg:block'} max-lg:absolute max-lg:top-[60px] max-lg:left-0 max-lg:bg-[#333] max-lg:p-1 lg:px-[40px] max-lg:z-50`}>                
-				<li className="max-lg:text-center  lg:inline-block lg:mr-[10px]"><Link className={`py-10 px-[10px] text-white hover:underline hover:decoration-orange-500 ${path==="/"?"underline decoration-blue-500":""}`} href="/">Home</Link></li>
-				<li className=" max-lg:text-center lg:inline-block lg:mr-[10px]"><Link className={`py-5 px-[10px] text-white hover:underline hover:decoration-orange-500 ${path==="/about"?"underline decoration-blue-500":""}`} href="/about">About</Link></li>
-				<li className="max-lg:text-center lg:inline-block lg:mr-[10px]"><Link className={`py-5 px-[10px] text-white hover:underline hover:decoration-orange-500 ${path==="/contact"?"underline decoration-blue-500":""}`} href="/contact">Contact</Link></li>
-                <li className="max-lg:text-center lg:inline-block lg:mr-[10px]"><Link className={`py-5 px-[10px] text-white hover:underline hover:decoration-orange-500 ${path==="/clients"?"underline decoration-blue-500":""}`} href="/contact">Clients</Link></li>
-                <li className="max-lg:text-center lg:inline-block lg:mr-[10px]"><Link className={`py-5 px-[10px] text-white hover:underline hover:decoration-orange-500 ${path==="/partner"?"underline decoration-blue-500":""}`} href="/contact">Partners</Link></li>
+                <ul className={`flex justify-center  max-lg:w-full   max-lg:flex-col ${!show ? 'max-lg:hidden' : 'max-lg:block'} max-lg:absolute max-lg:top-[60px] max-lg:left-0 max-lg:bg-[#333] max-lg:p-4 lg:px-[40px] max-lg:z-50`}>                
+				<li className="max-lg:text-center max-lg:py-4  lg:inline-block lg:mr-[10px]"><Link className={`py-10 px-[10px] text-white hover:underline hover:decoration-orange-500 ${path==="/"?"underline decoration-blue-500":""}`} href="/">Home</Link></li>
+				<li className=" max-lg:text-center max-lg:py-4 lg:inline-block lg:mr-[10px]"><Link className={`py-5 px-[10px] text-white hover:underline hover:decoration-orange-500 ${path==="/about"?"underline decoration-blue-500":""}`} href="/about">About</Link></li>
+				<li className="max-lg:text-center max-lg:py-4 lg:inline-block lg:mr-[10px]"><Link className={`py-5 px-[10px] text-white hover:underline hover:decoration-orange-500 ${path==="/contact"?"underline decoration-blue-500":""}`} href="/contact">Contact</Link></li>
+                <li className="max-lg:text-center max-lg:py-4 lg:inline-block lg:mr-[10px]"><Link className={`py-5 px-[10px] text-white hover:underline hover:decoration-orange-500 ${path==="/clients"?"underline decoration-blue-500":""}`} href="/contact">Clients</Link></li>
+                <li className="max-lg:text-center max-lg:py-4 lg:inline-block lg:mr-[10px]"><Link className={`py-5 px-[10px] text-white hover:underline hover:decoration-orange-500 ${path==="/partner"?"underline decoration-blue-500":""}`} href="/contact">Partners</Link></li>
 
         	</ul>
             
