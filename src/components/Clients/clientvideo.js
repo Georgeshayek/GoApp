2f069351c6691eb8035546449c4b62564d1270bd
Georgeshayek <georgeshayek85@gmail.com>
2024-03-21 12:59:21 +0200

    adding popup enhance in the client page

--- src/components/Clients/clientvideo.js
+++ src/components/Clients/clientvideo.js
@@ -1,41 +1,59 @@
 "use client"
-import { useState } from "react"
+import { useState,useEffect,useRef } from "react"
 import ModalVideo from 'react-modal-video';
 import '../../app/globals.css'
 import Image from "next/image";
+import "react-modal-video/scss/modal-video.scss";
 
 const ClientVideo=()=>{
     const [isOpen,setOpen]=useState(false)
+    const handler=()=>{
+        setOpen(false)
+    }
 
+     useEffect(() => {
+    // Function to disable scrollbar when the overlay is open
+    const disableBodyScroll = () => {
+      document.body.style.overflow = 'hidden';
+    };
+
+    // Function to enable scrollbar when the overlay is closed
+    const enableBodyScroll = () => {
+      document.body.style.overflow = 'auto';
+    };
+
+    // Check if the overlay is open and call the appropriate function to disable/enable scrollbar
+    if (isOpen) {
+      disableBodyScroll();
+    } else {
+      enableBodyScroll();
+    }
+    // Check if the iframe is available
+   
+
+    // Cleanup function to enable scrollbar when the component unmounts
+    return () => {
+      enableBodyScroll();
+    };
+  }, [isOpen]);
     return(
-
-        <div className="relative"> 
-        <div className={`fixed top-0 left-0 w-full h-full bg-black bg-opacity-70 ${isOpen ? 'block' : 'hidden'}`} />
-    <div className="container mx-auto">
-
+<>  
     <ModalVideo
     channel="youtube"
     youtube={{ mute: 0, autoplay: 1 }}
     isOpen={isOpen}
     videoId="IdejM6wCkxA"
     onClose={() => setOpen(false)}
-    classNames={{
-        modalVideo: 'custom-modal-video',
-        modalVideoClose: 'custom-modal-video-close',
-        modalVideoBody: 'custom-modal-video-body',
-        modalVideoInner: 'custom-modal-video-inner',
-        modalVideoIframeWrap: 'custom-modal-video-iframe-wrap',
-        modalVideoCloseBtn: 'custom-modal-video-close-btn'
-      }}  />
+   
+        />
 
 <div className="flex justify-center items-center py-8">
-      <Image src="/Dubai.png" width={500} height={400} className={`relative ${isOpen?"brightness-50":""}`}/> 
+      <Image src="/Dubai.png" width={500} height={400} className={` max-lg:px-4 relative ${isOpen?"brightness-50":""}`}/> 
        <button className="absolute  text-white" onClick={() => setOpen(true)}>
     VIEW DEMO
   </button>
   </div>
-  </div>
-    </div>
+</>    
     )
 }
 export default ClientVideo
