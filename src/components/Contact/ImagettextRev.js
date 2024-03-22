146622d3adad1de6e6ecccc07df8f3136773be00
Georgeshayek <georgeshayek85@gmail.com>
2024-03-22 11:41:10 +0200

    contact animation

--- src/components/Contact/ImagettextRev.js
+++ src/components/Contact/ImagettextRev.js
@@ -1,13 +1,46 @@
+"use client"
 import Image from "next/image"
-
+import gsap from "gsap"
+import { useRef,useEffect } from "react"
 const ImageTextRev=(props)=>{
+    const imgref=useRef()
+    const textref=useRef()
+    useEffect(()=>{
+        const image=imgref.current
+        const text=textref.current
+        gsap.fromTo(image, {
+            opacity: 0,
+            x: `${props.reverse?"-100%":"100%"}`
+        }, {
+            opacity: 1,
+            x: 0,
+            duration: 1.5,
+            ease: 'power3.out',scrollTrigger:{
+                trigger:image
+            }
+        });
+    
+        // Animation for the text with a delay
+        gsap.fromTo(text, {
+            opacity: 0,
+            x: `${props.reverse?"100%":"-100%"}`
+        }, {
+            opacity: 1,
+            x: 0,
+            duration: 1.5,
+            ease: 'power3.out',
+            delay: 1 ,scrollTrigger:{
+                trigger:text
+            }
+        });
 
+    },[])
     return(
         <>
-        <div className="container mx-auto">
+        <div className="container mx-auto py-10">
         <div className={`lg:relative  flex ${props.reverse? "lg:flex-row-reverse ":""} max-lg:flex-col max-lg:items-center max-lg:justify-center  max-lg:px-2 lg:px-20 py-8`}>
-            <Image src={props.imageUrl} width={800} height={300} className=" brightness-50	 max-lg:max-w-[250px] "alt="furnuture"/>
-                <h1 className={ `lg:absolute  top-[40%] max-lg:py-3 ${props.reverse?"lg:left-[15%]":"lg:left-[55%]"} text-[2rem] lg:max-w-[360px]  max-lg:text-[11px] ${props.reverse?"-left-16":"-left-6"}`}>{props.title} </h1>
+            <Image ref={imgref} src={props.imageUrl} width={800} height={300} className=" brightness-50	 max-lg:max-w-[250px] "alt="furnuture"/>
+                <h1 ref={textref} className={ `lg:absolute  top-[40%] max-lg:py-3 ${props.reverse?"lg:left-[15%]":"lg:left-[55%]"} text-[2rem] lg:max-w-[360px]  max-lg:text-[11px] ${props.reverse?"-left-16":"-left-6"}`}>{props.title} </h1>
             
             </div>
         </div>
--- src/components/Shared/navbar.js
+++ src/components/Shared/navbar.js
@@ -9,7 +9,9 @@ const Navbar=()=>{
     const nav=useRef();
     useEffect(()=>{
         const el=nav.current
-        gsap.fromTo(el,{duration:2.5,ease:"back.in",y:100},{duration:2.5,ease:"back.out",y:0})
+        gsap.fromTo(el,{duration:2.5,ease:"back.in",y:100 },{duration:2.5,ease:"back.out",y:0,onComplete:()=>{
+            gsap.to(el,{zIndex:50})
+        } })
     },[])
     const path=usePathname();
     const [show, setShow] = useState(false);
@@ -29,7 +31,7 @@ const Navbar=()=>{
                     <Link  href="/"><Image src="/logo.png" alt=" Logo" width={100} height={50}/></Link>
                 </div>
                 <button id="burger-btn" className="  block text-white lg:hidden" onClick={toggleShow}>&#9776;</button>
-                <ul className={`flex justify-center  max-lg:w-full   max-lg:flex-col ${!show ? 'max-lg:hidden' : 'max-lg:block'} max-lg:absolute max-lg:top-[60px] max-lg:left-0 max-lg:bg-[#000] max-lg:p-4 lg:px-[40px] max-lg:z-50`}>                
+                <ul className={`flex justify-center  max-lg:w-full  max-lg:z-50  max-lg:flex-col ${!show ? 'max-lg:hidden' : 'max-lg:block'} max-lg:absolute max-lg:top-[60px] max-lg:left-0 max-lg:bg-[#000] max-lg:p-4 lg:px-[40px] max-lg:z-50`}>                
 				<li className="max-lg:text-center max-lg:py-4  lg:inline-block lg:mr-[10px]"><Link onClick={toggleOff}  className={`py-10 px-[10px] text-white hover:underline hover:decoration-orange-500 ${path==="/"?"underline decoration-blue-500":""}`} href="/">Home</Link></li>
 				<li className=" max-lg:text-center max-lg:py-4 lg:inline-block lg:mr-[10px]"><Link onClick={toggleOff}  className={`py-5 px-[10px] text-white hover:underline hover:decoration-orange-500 ${path==="/about"?"underline decoration-blue-500":""}`} href="/about">About</Link></li>
 				<li className="max-lg:text-center max-lg:py-4 lg:inline-block lg:mr-[10px]"><Link onClick={toggleOff}  className={`py-5 px-[10px] text-white hover:underline hover:decoration-orange-500 ${path==="/contact"?"underline decoration-blue-500":""}`} href="/contact">Contact</Link></li>
