c91b95488fae9980bd815e16108396d4b2226ef2
Georgeshayek <georgeshayek85@gmail.com>
2024-03-22 12:12:25 +0200

    contact animation3

--- src/components/Contact/ImagettextRev.js
+++ src/components/Contact/ImagettextRev.js
@@ -7,18 +7,22 @@ gsap.registerPlugin(ScrollTrigger);
 const ImageTextRev=(props)=>{
     const imgref=useRef()
     const textref=useRef()
+    const wrapper =useRef()
     useEffect(()=>{
         const image=imgref.current
         const text=textref.current
+        const wrap= wrapper.current
         gsap.fromTo(image, {
             opacity: 0,
             x: `${props.reverse?"-45%":"45%"}`
         }, {
             opacity: 1,
             x: 0,
-            duration: 1.5,
+            duration: 1,
             ease: 'power3.out',scrollTrigger:{
-                trigger:image
+                trigger:wrap,
+                start:"top top",
+                
             }
         });
     
@@ -28,10 +32,11 @@ const ImageTextRev=(props)=>{
         }, {
             opacity: 1,
             x: 0,
-            duration: 1.5,
+            duration: 1,
             ease: 'power3.out',
-            delay: 1 ,scrollTrigger:{
-                trigger:text
+            delay: 0.5 ,scrollTrigger:{
+                trigger:wrap,
+                start:"top top",
             }
         });
 
@@ -39,7 +44,7 @@ const ImageTextRev=(props)=>{
     return(
         <>
         <div className="container mx-auto py-10">
-        <div className={`lg:relative  flex ${props.reverse? "lg:flex-row-reverse ":""} max-lg:flex-col max-lg:items-center max-lg:justify-center  max-lg:px-2 lg:px-20 py-8`}>
+        <div ref={wrapper} className={` lg:relative  flex ${props.reverse? "lg:flex-row-reverse ":""} max-lg:flex-col max-lg:items-center max-lg:justify-center  max-lg:px-2 lg:px-20 py-8`}>
             <Image ref={imgref} src={props.imageUrl} width={800} height={300} className=" brightness-50	 max-lg:max-w-[250px] "alt="furnuture"/>
                 <h1 ref={textref} className={ `lg:absolute  top-[40%] max-lg:py-3 ${props.reverse?"lg:left-[15%]":"lg:left-[55%]"} text-[2rem] lg:max-w-[360px]  max-lg:text-[11px] ${props.reverse?"-left-16":"-left-6"}`}>{props.title} </h1>
             
