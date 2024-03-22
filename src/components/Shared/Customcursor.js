c08a18bf1650da8abce5617192e513505b7d3663
Georgeshayek <georgeshayek85@gmail.com>
2024-03-22 14:09:38 +0200

    cursor animaition

---
+++ src/components/Shared/Customcursor.js
@@ -0,0 +1,54 @@
+"use client"
+import { useRef, useEffect } from 'react';
+import { gsap } from 'gsap';
+
+const CustomCursor = () => {
+    const cursorRef = useRef(null);
+
+    useEffect(() => {
+        const cursor = cursorRef.current;
+        const textElements = document.querySelectorAll('p, span, h1, h2, h3, h4, h5, h6, a, button');
+
+        const moveCursor = (e) => {
+            gsap.set(cursor, {
+                x: e.clientX,
+                y: e.clientY,
+            });
+        };
+
+        const enterText = () => {
+            gsap.to(cursor, { backgroundColor: 'white',opacity:0.5, borderColor: 'black' });
+
+        };
+
+        const leaveText = () => {
+            gsap.to(cursor, { backgroundColor: 'transparent', borderColor: 'blue' });
+
+        };
+
+        document.addEventListener('mousemove', moveCursor);
+
+        textElements.forEach(element => {
+            element.addEventListener('mouseenter', enterText);
+            element.addEventListener('mouseleave', leaveText);
+        });
+
+        return () => {
+            document.removeEventListener('mousemove', moveCursor);
+
+            textElements.forEach(element => {
+                element.removeEventListener('mouseenter', enterText);
+                element.removeEventListener('mouseleave', leaveText);
+            });
+        };
+    }, []);
+
+    return (
+        <div
+            ref={cursorRef}
+            className="w-12 h-12 fixed top-0 left-0 border-4 z-50 border-blue-500 rounded-full cursor-none"
+        ></div>
+    );
+};
+
+export default CustomCursor;
