66c78b2380dfcf0dbf589268167287a7c89c63cd
Georgeshayek <georgeshayek85@gmail.com>
2024-03-28 11:10:21 +0200

    adding category to partner page1

--- src/components/partners/categoryfilter.js
+++ src/components/partners/categoryfilter.js
@@ -74,23 +74,23 @@ const CategoryFilter=()=>{
         <>
         <div className="px-4 py-6">
             <div className=" flex max-lg:flex-col justify-between items-center py-8 px-4">
-                <div onClick={onOptionHandler.bind(null,"all category")} className="flex flex-col justify-center w-full items-center">
-                    <h1 className={`${option=="all category"?"font-bold":""}`}>All category</h1>
+                <div onClick={onOptionHandler.bind(null,"all category")} className="flex flex-col justify-center w-full items-center max-lg:py-3">
+                    <h1 className={`${option=="all category"?"font-bold text-xl":""}`}>All Category</h1>
                     <div className={`${option=="all category"?"bg-white w-full ease-in duration-300":"bg-gray-500 w-10"}  h-1`}></div>
                 </div>
-                <div onClick={onOptionHandler.bind(null,"tech")} className="flex flex-col w-full justify-center items-center">
-                    <h1 className={`${option=="tech"?"font-bold":""}`}>Tech</h1>
+                <div onClick={onOptionHandler.bind(null,"tech")} className="flex flex-col w-full justify-center items-center  max-lg:py-3">
+                    <h1 className={`${option=="tech"?"font-bold text-xl":""}`}>Tech</h1>
                     <div className={`${option=="tech"?"bg-white w-full ease-in duration-300":"bg-gray-500 w-10"}  h-1`}></div>
                 </div>
-                <div onClick={onOptionHandler.bind(null,"art")} className="flex flex-col w-full justify-center items-center">
-                    <h1 className={`${option=="art"?"font-bold":""}`}>Art</h1>
+                <div onClick={onOptionHandler.bind(null,"art")} className="flex flex-col w-full justify-center items-center  max-lg:py-3">
+                    <h1 className={`${option=="art"?"font-bold text-xl":""}`}>Art</h1>
                     <div className={`${option=="art"?"bg-white w-full ease-in duration-300":"bg-gray-500 w-10"}  h-1`}></div>
                 </div>
             </div>
 
             <div className="grid max-lg:grid-cols-1 grid-cols-2 gap-2">
             {arr.map(mov=>(
-                <div className="flex flex-col col-span-1 justify-center items-center">
+                <div className="flex flex-col col-span-1 justify-center py-3 items-center">
                     <Image src={mov.imgurl} alt="img" width={400} height={300}/>
                      <h1 className="font-bold">{mov.title}</h1>
                 </div>
